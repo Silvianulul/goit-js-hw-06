2bc22f7f26cd5d56893a11a98a8e1d5c8e53be83
Silvianulul <silvi_mitrica@yahoo.com>
2024-05-14 18:45:13 +0300

    js-hw-06

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formElements = loginForm.elements;
+
+  const formData = {};
+
+  for (let i = 0; i < formElements.length; i++) {
+    const element = formElements[i];
+
+    if (element.tagName === "INPUT") {
+      if (element.value.trim() === "") {
+        alert("All fields must be filled out");
+        return;
+      }
+
+      formData[element.name] = element.value;
+    }
+  }
+
+  console.log(formData);
+
+  loginForm.reset();
+});
